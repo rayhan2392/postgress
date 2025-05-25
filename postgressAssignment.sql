bcac2cbbd8bd88d050b1e84f3802d6af5a00f9a1
rayhan2392 <rkshawnso@gmail.com>
2025-05-26 03:52:00 +0600

    4 problems solved

---
+++ postgressAssignment.sql
@@ -0,0 +1,79 @@
+-- Active: 1747419213330@@localhost@5432@conservation_db
+CREATE DATABASE conservation_db
+
+CREATE TABLE rangers (
+    ranger_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    region VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(100) NOT NULL,
+    scientific_name VARCHAR(100) NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE sightings (
+    sighting_id SERIAL PRIMARY KEY,
+    ranger_id INTEGER REFERENCES rangers(ranger_id),
+    species_id INTEGER REFERENCES species(species_id),
+    sighting_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    location VARCHAR(100) NOT NULL,
+    notes TEXT
+);
+
+INSERT INTO rangers (name, region) VALUES
+('Alice Green', 'Northern Hills'),
+('Bob White', 'River Delta'),
+('Carol King', 'Mountain Range');
+
+SELECT * FROM rangers
+
+INSERT INTO species (common_name, scientific_name, discovery_date, conservation_status) VALUES
+('Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
+('Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
+('Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
+('Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
+
+
+SELECT * FROM species
+
+INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes) VALUES
+(1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
+(2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
+(3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
+(1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
+
+
+SELECT * FROM sightings
+
+
+
+
+
+
+
+                          --problem:01
+
+INSERT INTO rangers (name,region) VALUES ( 'Derek Fox' ,'Coastal Plains')
+
+
+                        --Proble 02
+SELECT COUNT(DISTINCT species_id) AS unique_species_id
+FROM sightings;
+
+
+                 --problem 03
+SELECT * FROM sightings
+WHERE location like '%Pass%'
+
+
+--                  Probelm -04
+SELECT name, count(sighting_id) as total_sightings FROM rangers
+JOIN sightings USING(ranger_id)
+GROUP BY name
+
+--       problem-05
+
